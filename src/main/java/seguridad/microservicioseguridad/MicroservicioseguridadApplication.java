a91aa02c151a6aa85720c55f73461a632313884a
adreno87 <adreno87xd@gmail.com>
2022-11-01 19:10:15 -0500

    inicio

---
+++ src/main/java/seguridad/microservicioseguridad/MicroservicioseguridadApplication.java
@@ -0,0 +1,13 @@
+package seguridad.microservicioseguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioseguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioseguridadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seguridad/microservicioseguridad/MicroservicioseguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package seguridad.microservicioseguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioseguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
