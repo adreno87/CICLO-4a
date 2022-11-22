29d56de83b701f629cd2b66da4595dd71ae5b161
Ana Valencia <avalenciasolano1@gmail.com>
2022-11-22 00:23:00 -0500

    Correcciones

--- src/main/java/seguridad/microservicioseguridad/Controladores/ControladorRolPermiso.java
+++ src/main/java/seguridad/microservicioseguridad/Controladores/ControladorRolPermiso.java
@@ -7,43 +7,74 @@ import seguridad.microservicioseguridad.Repositorios.RepositorioPermiso;
 import seguridad.microservicioseguridad.Repositorios.RepositorioRol;
 import seguridad.microservicioseguridad.Repositorios.RepositorioRolPermiso;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/rolpermiso")
 public class ControladorRolPermiso {
     @Autowired
-    RepositorioPermiso miRepoPermiso;
+    private RepositorioRolPermiso miRepoRolPermiso;
     @Autowired
-    RepositorioRol miRepoRol;
+    private RepositorioPermiso miRepoPermiso;
     @Autowired
-    RepositorioRolPermiso miRepoRolPermiso;
+    private RepositorioRol miRepoRol;
 
-    @PostMapping("/crear/{idRol}/permiso/{idPermiso}")
-    public RolPermiso crearRolPermiso(@PathVariable String idRol, @PathVariable String idPermiso){
-        Rol rolConsulta = miRepoRol.findById(idRol).orElse(null);
-        Permiso permisoConsulta = miRepoPermiso.findById(idPermiso).orElse(null);
-        RolPermiso rolPermiso = new RolPermiso(rolConsulta,permisoConsulta);
-        return miRepoRolPermiso.save(rolPermiso);
+    @GetMapping("")
+    public List<RolPermiso> index(){
+        return this.miRepoRolPermiso.findAll();
     }
-
-    @GetMapping("/listar")
-    public List<RolPermiso> listarRolPermiso(){
-        return miRepoRolPermiso.findAll();
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("rol/{id_rol}/permiso/{id_permiso}")
+    public RolPermiso create(@PathVariable String id_rol,@PathVariable
+    String id_permiso) {
+        RolPermiso nuevo = new RolPermiso();
+        Rol elRol = this.miRepoRol.findById(id_rol).get();
+        Permiso elPermiso = this.miRepoPermiso.findById(id_permiso).get();
+        if (elRol != null && elPermiso != null) {
+            nuevo.set_idPermiso(elPermiso);
+            nuevo.set_idRol(elRol);
+            return this.miRepoRolPermiso.save(nuevo);
+        } else {
+            return null;
+        }
     }
-
-    @PutMapping("/actualizar/{idRol}/permiso/{idPermiso}")
-    public void asignarRolPermiso(){
-
+    @GetMapping("{id}")
+    public RolPermiso show(@PathVariable String id) {
+        RolPermiso permisosRolesActual = this.miRepoRolPermiso
+                .findById(id)
+                .orElse(null);
+        return permisosRolesActual;
     }
-
-    @DeleteMapping("/eliminar/{idRolPermiso}")
-    public void eliminarRolPermiso(){
-
+    @PutMapping("{id}/rol/{id_rol}/permiso/{id_permiso}")
+    public RolPermiso update(@PathVariable String id,@PathVariable
+    String id_rol,@PathVariable String id_permiso){
+        RolPermiso permisosRolesActual=this.miRepoRolPermiso
+                .findById(id)
+                .orElse(null);
+        Rol elRol=this.miRepoRol.findById(id_rol).get();
+        Permiso
+                elPermiso=this.miRepoPermiso.findById(id_permiso).get();
+        if(permisosRolesActual!=null && elPermiso!=null && elRol!=null){
+            permisosRolesActual.set_idPermiso(elPermiso);
+            permisosRolesActual.set_idRol(elRol);
+            return
+                    this.miRepoRolPermiso.save(permisosRolesActual);
+        }else{
+            return null;
+        }
     }
-
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        RolPermiso permisosRolesActual=this.miRepoRolPermiso
+                .findById(id)
+                .orElse(null);
+        if (permisosRolesActual!=null){
+            this.miRepoRolPermiso.delete(permisosRolesActual);
+        }
+    }
     @GetMapping("validar-permiso/rol/{id_rol}")
     public RolPermiso getPermiso(@PathVariable String id_rol, @RequestBody Permiso infoPermiso) {
         Permiso elPermiso = this.miRepoPermiso.getPermiso(infoPermiso.getUrl(), infoPermiso.getMetodo());
